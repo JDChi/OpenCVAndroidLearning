0a138609effac63d84d808b99b46e9101da779dd
jdchi <623893416@qq.com>
2021-05-07 12:03:42 +0800

    add permission request

--- app/src/main/java/com/example/opencvlearning/MainActivity.java
+++ app/src/main/java/com/example/opencvlearning/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.opencvlearning;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import com.example.opencvlearning.demo.HelloOpenCVSampleActivity;
+import com.permissionx.guolindev.PermissionCollection;
+import com.permissionx.guolindev.PermissionX;
+import com.permissionx.guolindev.callback.ExplainReasonCallback;
+import com.permissionx.guolindev.callback.RequestCallback;
+import com.permissionx.guolindev.request.ExplainScope;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private AppCompatButton helloButton;
@@ -19,6 +27,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         helloButton = findViewById(R.id.bt_hello);
         helloButton.setOnClickListener(this);
+
+        requestPermissions();
+    }
+
+    private void requestPermissions() {
+        PermissionCollection permissionCollection = PermissionX.init(this);
+        permissionCollection.permissions(Manifest.permission.CAMERA)
+                .onExplainRequestReason((scope, deniedList) ->
+                        scope.showRequestReasonDialog(deniedList, "Core fundamental are based on these permissions", "Ok", "Cancel")).request(new RequestCallback() {
+            @Override
+            public void onResult(boolean allGranted, List<String> grantedList, List<String> deniedList) {
+
+            }
+        });
     }
 
     @Override
